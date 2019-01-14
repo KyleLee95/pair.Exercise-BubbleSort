91583fb36f103c1bfc1095b62c6c2c7c33658886
Kyle Lee <Kylecrlee95@gmail.com>
2019-01-14 13:38:30 -0600

    added merge sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,28 +1,40 @@
 function bubbleSort(arr) {
   debugger;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      let cur = arr[j];
-      let next = arr[j + 1];
-      if (cur !== undefined && next !== undefined) {
-        let newArr = swap(cur, next);
-        arr[j] = newArr[0];
-        arr[j + 1] = newArr[1];
+  if (arr.length === 0) {
+    return undefined;
+  }
+  if (arr.length === 1) {
+    return arr;
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        let cur = arr[j];
+        let next = arr[j + 1];
+        //Breaks right here after the first call. because cur is defined but next = undefined
+        //so it won't step into the loop
+        if (next === undefined) {
+          continue;
+        } else if (cur !== undefined && next !== undefined) {
+          let newArr = swap(cur, next);
+          arr[j] = newArr[0];
+          arr[j + 1] = newArr[1];
+        }
       }
+      console.log(arr);
     }
-    console.log(arr);
     return arr;
   }
+}
 
-  function swap(cur, next) {
-    debugger;
-    let retArr = [];
-    if (cur > next) {
-      let temp = cur;
-      cur = next;
-      next = temp;
-      retArr = [cur, next];
-    }
-    return retArr;
+function swap(cur, next) {
+  let retArr = [];
+  if (cur > next) {
+    let temp = cur;
+    cur = next;
+    next = temp;
+    retArr = [cur, next];
+  } else {
+    retArr = [cur, next];
   }
+  return retArr;
 }
