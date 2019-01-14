cedc8e2d8e0cc278572fd494220c37032a7e87b8
Kyle Lee <Kylecrlee95@gmail.com>
2019-01-14 13:58:19 -0600

    working on merge

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,5 +1,15 @@
 describe("Split Array function", function() {
+  let test_sort_arr = [1, 2, 3, 4, 5, 6, 7, 0];
+  let sorted_arr = [[1, 2, 3, 4], [5, 6, 7, 0]];
   it("is able to split an array into two halves", function() {
     // your code here
+    const splitArr = split(test_sort_arr);
+    expect(splitArr).toEqual(sorted_arr);
+  });
+});
+
+describe("Merge function", function() {
+  it("is able to merge two sorted arrays into one sorted array", function() {
+    // test the merging algorithm
   });
 });
