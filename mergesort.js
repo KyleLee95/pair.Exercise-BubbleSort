170c0fe46dfc9d6c5f3c1ea66bbfee4fa806479d
Kyle Lee <Kylecrlee95@gmail.com>
2019-01-14 14:45:01 -0600

    checking for merge base cases

--- mergesort.js
+++ mergesort.js
@@ -1,16 +1,24 @@
 function split(wholeArray) {
   let midpoint = Math.floor(wholeArray.length / 2);
+  if (wholeArray.length === 1) {
+    //done splitting
+    return wholeArray;
+  }
   let left = wholeArray.slice(0, midpoint);
   let right = wholeArray.slice(midpoint, wholeArray.length); //right larger than the left
-
   return [left, right];
 }
 
 console.log(split([1, 2, 3, 4, 5, 6, 7]));
 
 function merge(splitArray) {
-  let cur = splitArray[0][0]
-  let next = splitArray[1][0]
+  if (splitArray.length === 1) {
+    return splitArray[0]; //splitArray[0]
+  }
+
+  //Merging when splitArray.length > 1
+  let cur = splitArray[0][0];
+  let next = splitArray[1][0];
   let retArr = [];
   if (cur > next) {
     let temp = cur;
@@ -23,4 +31,12 @@ function merge(splitArray) {
   return retArr;
 }
 
-console.log(merge([[2], [1]]))
+console.log(split([1]));
+
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return merge(arr);
+  }
+}
+
+console.log(merge([[1]]));
