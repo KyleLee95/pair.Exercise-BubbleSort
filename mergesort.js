fdfaeca83be0730bf92d4141f2a77f881ef15eb3
jan <janusbased@gmail.com>
2019-01-14 14:16:43 -0600

    got merge spec to pass

--- mergesort.js
+++ mergesort.js
@@ -7,3 +7,20 @@ function split(wholeArray) {
 }
 
 console.log(split([1, 2, 3, 4, 5, 6, 7]));
+
+function merge(splitArray) {
+  let cur = splitArray[0][0]
+  let next = splitArray[1][0]
+  let retArr = [];
+  if (cur > next) {
+    let temp = cur;
+    cur = next;
+    next = temp;
+    retArr = [cur, next];
+  } else {
+    retArr = [cur, next];
+  }
+  return retArr;
+}
+
+console.log(merge([[2], [1]]))
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -2,7 +2,6 @@ describe("Split Array function", function() {
   let test_sort_arr = [1, 2, 3, 4, 5, 6, 7, 0];
   let sorted_arr = [[1, 2, 3, 4], [5, 6, 7, 0]];
   it("is able to split an array into two halves", function() {
-    // your code here
     const splitArr = split(test_sort_arr);
     expect(splitArr).toEqual(sorted_arr);
   });
@@ -10,6 +9,9 @@ describe("Split Array function", function() {
 
 describe("Merge function", function() {
   it("is able to merge two sorted arrays into one sorted array", function() {
-    // test the merging algorithm
+    let test_split_arr = [[2], [1]];
+    let merged_arr = [1, 2];
+    const splitArr = merge(test_split_arr);
+    expect(splitArr).toEqual(merged_arr);
   });
 });
